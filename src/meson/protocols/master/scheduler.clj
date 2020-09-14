7af037a064826ca8fc40bbe3b81e804d127208e4
wdhowe <wdhowe@protonmail.com>
2020-09-14 11:35:16 -0500

    issue/95/scheduler-calls-missing: Implement missing scheduler calls
    Added the following scheduler protocols and calls:
    
    * acknowledge-operation-status
    * reconcile-operations
    * suppress
    * update-framework
    
    Also enabled clj-fmt on save, which made the formatting changes.

--- src/meson/protocols/master/scheduler.clj
+++ src/meson/protocols/master/scheduler.clj
@@ -5,36 +5,61 @@
   * http://mesos.apache.org/documentation/latest/scheduler-http-api/")
 
 (defprotocol IScheduler
-  ""
-  (accept [this payload stream-id framework-id] [this payload stream-id framework-id content-type]
+  "Scheduler API Calls accepted by the master."
+  (accept [this payload stream-id framework-id]
+    [this payload stream-id framework-id content-type]
     "Sent by the scheduler when it accepts offer(s) sent by the master. The
     `ACCEPT` request includes the type of operations (e.g., launch task,
     reserve resources, create volumes) that the scheduler wants to perform on
     the offers.")
-  (acknowledge [this payload stream-id framework-id] [this payload stream-id framework-id content-type]
+  (acknowledge [this payload stream-id framework-id]
+    [this payload stream-id framework-id content-type]
     "Sent by the scheduler to acknowledge a status update.")
-  (decline [this payload stream-id framework-id] [this payload stream-id framework-id content-type]
+  (acknowledge-operation-status [this payload stream-id framework-id]
+    [this payload stream-id framework-id content-type]
+    "Sent by the scheduler to acknowledge an operation status update.")
+  (decline [this payload stream-id framework-id]
+    [this payload stream-id framework-id content-type]
     "Sent by the scheduler to explicitly decline offer(s) received. Note that
     this is same as sending an `ACCEPT` call with no operations.")
-  (kill-task [this payload stream-id framework-id] [this payload stream-id framework-id content-type]
+  (kill-task [this payload stream-id framework-id]
+    [this payload stream-id framework-id content-type]
     "Sent by the scheduler to kill a specific task. If the scheduler has a
     custom executor, the kill is forwarded to the executor; it is up to the
     executor to kill the task and send a `TASK_KILLED` (or `TASK_FAILED`)
     update.")
-  (message [this payload stream-id framework-id] [this payload stream-id framework-id content-type]
+  (message [this payload stream-id framework-id]
+    [this payload stream-id framework-id content-type]
     "Sent by the scheduler to send arbitrary binary data to the executor.")
-  (reconcile [this payload stream-id framework-id] [this payload stream-id framework-id content-type]
+  (reconcile [this payload stream-id framework-id]
+    [this payload stream-id framework-id content-type]
     "Sent by the scheduler to query the status of non-terminal tasks.")
-  (request [this payload stream-id framework-id] [this payload stream-id framework-id content-type]
+  (reconcile-operations [this payload stream-id framework-id]
+    [this payload stream-id framework-id content-type]
+    "Sent by the scheduler to query the status of non-terminal and
+     terminal-but-unacknowledged operations.")
+  (request [this payload stream-id framework-id]
+    [this payload stream-id framework-id content-type]
     "Sent by the scheduler to request resources from the master/allocator.")
-  (revive [this payload stream-id framework-id] [this payload stream-id framework-id content-type]
+  (revive [this payload stream-id framework-id]
+    [this payload stream-id framework-id content-type]
     "Sent by the scheduler to remove any/all filters that it has previously
     set via `ACCEPT` or `DECLINE` calls.")
-  (shutdown-executor [this payload stream-id framework-id] [this payload stream-id framework-id content-type]
+  (shutdown-executor [this payload stream-id framework-id]
+    [this payload stream-id framework-id content-type]
     "Sent by the scheduler to shutdown a specific custom executor.")
-  (subscribe [this payload] [this payload content-type]
+  (subscribe [this payload]
+    [this payload content-type]
     "This is the first step in the communication process between the scheduler
     and the master. This is also to be considered as subscription to the
     “/scheduler” events stream.")
-  (teardown [this stream-id framework-id] [this stream-id framework-id content-type]
-    "Sent by the scheduler when it wants to tear itself down."))
+  (suppress [this payload stream-id framework-id]
+    [this payload stream-id framework-id content-type]
+    "Sent by the scheduler when it doesn’t need offers for a given set of its roles.")
+  (teardown [this stream-id framework-id]
+    [this stream-id framework-id content-type]
+    "Sent by the scheduler when it wants to tear itself down.")
+  (update-framework [this payload]
+    [this payload content-type]
+    "Sent by the scheduler to change fields of its FrameworkInfo and/or the set of
+     suppressed roles."))
